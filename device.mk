90141f858c847afe3954595c410c8fd67010a030
Takeshiro04 <gianashtonreyes@gmail.com>
2020-06-24 14:41:40 +0000

    Prebuilt AIMP
    Signed-off-by: Takeshiro04 <gianashtonreyes@gmail.com>
    Co-authored-by: nparashar150 <nparashar150@gmail.com>

--- device.mk
+++ device.mk
@@ -1,4 +1,5 @@
-right (C) 2017-2019 The LineageOS Project
+#
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -116,3 +117,7 @@ $(call inherit-product, build/target/product/verity.mk)
 # Via
 PRODUCT_PACKAGES += \
     Via
+
+# AIMP
+PRODUCT_PACKAGES += \
+    AIMP
